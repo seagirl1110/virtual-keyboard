1e02ff4fee7d095aab296716d6884c98bf9bdaea
Marina Elizarova <seagirl1110@gmail.com>
2022-05-06 15:30:26 +0300

    refactor: initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, './src/script.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+}
