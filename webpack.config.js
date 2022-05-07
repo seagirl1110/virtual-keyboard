1bdc63d837a66d18068db30fdc84011d91db7029
Marina Elizarova <seagirl1110@gmail.com>
2022-05-07 20:23:40 +0300

    fix: setting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: "development",
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
     entry: {
         main: path.resolve(__dirname, './src/script.js')
     },
@@ -9,4 +19,8 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js'
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 }
